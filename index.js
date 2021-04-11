abea90bfe1106a4f3e61ffdc63c018610428a12d
o2sevruk <o2sevruk@gmail.com>
2021-04-11 14:14:27 +0300

    UI implementation begins

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// STORE
 function createStore(reducer) {
   // The store should have four parts
   // 1. The state
@@ -31,16 +32,87 @@ function createStore(reducer) {
   };
 }
 
-function todos(state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo]);
-  }
+// Actions
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
 
-  return state;
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+// Action Creators
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
 }
 
-const store = createStore(todos);
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+
+// REDUCERS
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) => (todo.id !== action.id
+        ? todo.id
+        : ({ ...todo, complete: !todo.complete })));
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+// APP
+const store = createStore(app);
 
 store.subscribe(() => {
-  console.log('The state was apdated!');
+  console.log('The current state is: ', store.getState());
 });
