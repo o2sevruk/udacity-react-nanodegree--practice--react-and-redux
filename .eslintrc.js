e17569c509a243805e7cc7f2db81198888e6c28c
Oleg Sevruk <o2sevruk@gmail.com>
2021-05-24 13:53:37 +0300

    Initial React

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
+  },
+};
